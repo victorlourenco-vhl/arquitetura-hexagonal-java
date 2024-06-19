b689d9bd84a33a202187a7ae8b79f696f42fc801
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-19 20:00:50 -0300

    build: Criando o projeto

---
+++ src/main/java/com/victor/arquitetura/hexagonal/ArquiteturaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.victor.arquitetura.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/arquitetura/hexagonal/ArquiteturaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.arquitetura.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
