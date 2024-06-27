2302d379aa0bfba6e3cbb304d43c73949b935c83
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-26 21:54:04 -0300

    refactor: substituindo a heração pela criação de atributos no AddressResponse

--- src/main/java/com/victor/arquitetura/hexagonal/adapters/out/client/response/AddressResponse.java
+++ src/main/java/com/victor/arquitetura/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -1,7 +1,42 @@
 package com.victor.arquitetura.hexagonal.adapters.out.client.response;
 
-import com.victor.arquitetura.hexagonal.application.core.domain.Address;
+public class AddressResponse {
+    private String street;
+    private String city;
+    private String state;
 
-public class AddressResponse extends Address {
+    public AddressResponse() {
 
+    }
+
+    public AddressResponse(String street, String city, String state) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
+
